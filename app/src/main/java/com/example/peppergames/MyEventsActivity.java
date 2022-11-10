9514d477a4e548cd73e4dc6f7050eb8068aefdfc
Pavan Yekbote <py@Pavans-MacBook-Air.local>
2022-11-10 00:53:35 -0600

    feat: creating empty activities for my events and all events + minor refactor

---
+++ app/src/main/java/com/example/peppergames/MyEventsActivity.java
@@ -0,0 +1,14 @@
+package com.example.peppergames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_events);
+    }
+}
