fd358741a281f42eb900dfb4d9a7afd63d535724
Chunyu-code <lanchenyi.babs@gmail.com>
2022-11-10 14:15:39 -0600

    pick a position

---
+++ app/src/main/java/com/example/peppergames/PickPosition.java
@@ -0,0 +1,14 @@
+package com.example.peppergames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PickPosition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_position);
+    }
+}
