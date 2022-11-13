23b9f58d1361e05ebdcff36c0bbbd0b4d81f0245
Pavan Yekbote <py@Pavans-MacBook-Air.local>
2022-11-12 18:00:53 -0600

    feat: generate events on database

--- app/src/main/java/com/example/peppergames/AllEventsActivity.java
+++ app/src/main/java/com/example/peppergames/AllEventsActivity.java
@@ -1,17 +1,73 @@
 package com.example.peppergames;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class AllEventsActivity extends EventsActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import com.example.peppergames.dto.Event;
+import com.example.peppergames.dto.TeamEnum;
+
+public class AllEventsActivity extends AppCompatActivity {
+
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_events);
+        LinearLayout layout = findViewById(R.id.event_outer_layout);
+        layout.removeAllViews();
 
-    }
+        CardView cardView;
+        int eventIndex = 0;
+//        toDo: generate events based on sorted value
+        for (Event event : Database.getEvents()) {
+            if (event.isPlaying()) {
+                continue;
+            }
 
-    public void addEvent() {
+            cardView = (CardView) getLayoutInflater().inflate(R.layout.event_card, null);
 
+            TextView textView = cardView.findViewById(R.id.event_game);
+            textView.setText(event.getGame());
+
+            textView = cardView.findViewById(R.id.event_skill);
+            textView.setText(String.format("Skills: %d", event.getSkillRating()));
+
+            textView = cardView.findViewById(R.id.event_skill);
+            textView.setText(String.format("Conduct: %d", event.getConductRating()));
+
+            textView = cardView.findViewById(R.id.event_date_time);
+            textView.setText(event.getDate());
+
+            int currentPlayers = 0;
+            for (TeamEnum key: event.getTeamPositions().keySet()) {
+                currentPlayers += event.getTeamPositions().get(key).keySet().size();
+            }
+
+            textView = cardView.findViewById(R.id.event_players);
+            textView.setText(String.format("%d/%d", currentPlayers, event.getMaxPlayers()));
+
+            textView = cardView.findViewById(R.id.event_location);
+            textView.setText(event.getLocation());
+
+            int finalEventIndex = eventIndex;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(AllEventsActivity.this, MyEventsActivity.class);
+                    intent.putExtra("event_index", finalEventIndex);
+                    startActivity(intent);
+                }
+            });
+
+            layout.addView(cardView);
+            eventIndex++;
+        }
     }
 }
---
+++ app/src/main/java/com/example/peppergames/Database.java
@@ -0,0 +1,62 @@
+package com.example.peppergames;
+
+import com.example.peppergames.dto.Event;
+import com.example.peppergames.dto.PositionEnum;
+import com.example.peppergames.dto.TeamEnum;
+import com.example.peppergames.dto.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Database {
+    private static final User Pavan = new User("Pavan Yekbote", 0, 3, "I'm a freshman. " +
+            "I have played football for about 3-4 years now on a regular basis. I'm looking to make some friends and have fun!",
+            22, 22, "Football", "22/01/2202");
+    private static final User Avram = new User("Avram", 5, 2, "I'm a freshman. " +
+            "I have played football for about 3-4 years now on a regular basis. I'm looking to make some friends and have fun!",
+            22, 22, "Football", "22/01/2202");
+    private static final User Pratt = new User("Pratt", 2, 1, "I'm a freshman. " +
+            "I have played football for about 3-4 years now on a regular basis. I'm looking to make some friends and have fun!",
+            22, 22, "Football", "22/01/2202");
+    private static final User Nora = new User("Nora", 2, 4, "I'm a freshman. " +
+            "I have played football for about 3-4 years now on a regular basis. I'm looking to make some friends and have fun!",
+            22, 22, "Football", "22/01/2202");
+    private static final User Smith = new User("Smith", 4, 3, "I'm a freshman. " +
+            "I have played football for about 3-4 years now on a regular basis. I'm looking to make some friends and have fun!",
+            22, 22, "Football", "22/01/2202");
+
+    private static final Map<PositionEnum, User> homePositions = new HashMap<>();
+    private static final Map<PositionEnum, User> awayPositions = new HashMap<>();
+
+    private static final Map<TeamEnum, Map<PositionEnum, User>> positions = new HashMap<>();
+
+    private static final List<Event> events = new ArrayList<>();
+    private static final Event amateurGame = new Event(
+            "Basketball", 2, 3, "6am, 11th Nov 2022", 12,
+            "ARC", false, positions);
+
+    private static final Event proGame = new Event(
+            "Football", 5, 4, "7am, 12th Nov 2022", 12
+            , "ARC", true, positions);
+
+    static {
+        homePositions.put(PositionEnum.GK, Avram);
+        homePositions.put(PositionEnum.RST, Pratt);
+        homePositions.put(PositionEnum.CM, Nora);
+
+        awayPositions.put(PositionEnum.LST, Pavan);
+        awayPositions.put(PositionEnum.CM, Smith);
+
+        positions.put(TeamEnum.HOME, homePositions);
+        positions.put(TeamEnum.AWAY, awayPositions);
+
+        events.add(amateurGame);
+        events.add(proGame);
+    }
+
+    public static List<Event> getEvents() {
+        return events;
+    }
+}
